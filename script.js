7316af0c05ddbe11f64c2e7eb5f365d532539994
MaxNux <maxi.nuxoll@icloud.com>
2025-04-01 21:39:41 +0200

    minor change

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let info = document.getElementById("info");
+const info = document.getElementById("info");
 const input = document.getElementById("input");
 
 function reverseString(str) {
-    return str.split("").reverse().join("");
+  return str.split("").reverse().join("");
 }
 
 function check() {
-    const value = input.value;
-    const reverse = reverseString(value);
+  const value = input.value;
+  const reverse = reverseString(value);
 
-    if (value === "") {
-        info.classList.remove("infoHidden", "infoTrue");
-        info.classList.add("infoFalse");
-        info.innerText = "You need to type something you Idiot";
-    } else if (value === reverse) {
-        info.classList.remove("infoHidden", "infoFalse");
-        info.classList.add("infoTrue");
-        info.innerText = "This is a P A L I N D R O M E!";
-    } else {
-        info.classList.remove("infoHidden", "infoTrue");
-        info.classList.add("infoFalse");
-        info.innerText = "This is not a Palindrome!";
-    }
+  if (value === "") {
+    info.classList.remove("infoHidden", "infoTrue");
+    info.classList.add("infoFalse");
+    info.innerText = "You need to type something you Idiot";
+  } else if (value === reverse) {
+    info.classList.remove("infoHidden", "infoFalse");
+    info.classList.add("infoTrue");
+    info.innerText = "This is a P A L I N D R O M E!";
+  } else {
+    info.classList.remove("infoHidden", "infoTrue");
+    info.classList.add("infoFalse");
+    info.innerText = "This is not a Palindrome!";
+  }
 
-    input.value = "";
+  input.value = "";
 }
